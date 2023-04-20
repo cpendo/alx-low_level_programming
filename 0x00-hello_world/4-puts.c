b6e172bcf220fc6dd730f5b1fbac515bf484bfa2
cpendo <pendoc15@gmail.com>
2023-04-20 06:39:50 -0700

    Prints the text using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main(void){
-
-puts("\"Programming is like building a multilingual puzzle\n");
-
-return 0;
+/**
+ * main - printing with puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
