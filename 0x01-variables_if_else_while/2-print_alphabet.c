9612723b53ffadcb66a2ebe32bb363370c6e6db8
cpendo <pendoc15@gmail.com>
2023-04-24 11:05:55 -0700

    Prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet
+ *
+ * Return: if successful 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i)
+	}
+	putchar('\n')
+	return (0);
+}
