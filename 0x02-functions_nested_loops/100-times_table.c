3c06159484d114342d1aa2283d9cbc39665e9b14
cpendo <pendoc15@gmail.com>
2023-04-26 04:24:09 -0700

    Print the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,10 +17,10 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		_putchar('0');
-		for (j = 1; j < n; j++)
+		for (j = 1; j <= n; j++)
 		{
 			result = i * j;
 			if (result < 10)
