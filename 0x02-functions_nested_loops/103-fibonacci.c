a965a3db3285aa672182d464ad253b489d76bb3c
cpendo <pendoc15@gmail.com>
2023-04-26 07:58:18 -0700

    Print sum of even terms in fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int fib1 = 1, fib2 = 2, sum, temp;
+
+	while (fib1 <= 4000000)
+	{
+		if (fib1 % 2 == 0)
+		{
+			sum += fib1;
+		}
+
+		temp = fib1;
+		fib1 += fib2;
+		fib2 = temp;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
