cb81a1ca212613aef41407128e65eb785e9b1c97
cpendo <pendoc15@gmail.com>
2023-04-27 14:42:26 -0700

    Print 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14, followed by a new line 10 times
+ *
+ * Return: 0 if successful
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('0' + j);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
