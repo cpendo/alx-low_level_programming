8971d21af417241bc000215fcb8698cf38605faa
cpendo <pendoc15@gmail.com>
2023-04-27 15:11:02 -0700

    Fix for loop to include nested if

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,7 +16,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + j);
+			if (j >= 10)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
 		}
 
 		_putchar('\n');
