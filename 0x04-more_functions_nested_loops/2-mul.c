d01070ecc0b29e5dec0fb6f7ea84d5d68ee1e889
cpendo <pendoc15@gmail.com>
2023-04-27 14:04:19 -0700

    Multiply a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiply a and b and return the result
+ *
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
