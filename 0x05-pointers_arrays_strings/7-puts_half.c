fb561f483363a9a8b2960298236f5c4928839f04
cpendo <pendoc15@gmail.com>
2023-05-02 14:00:28 -0700

    Print half of a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * puts_half - print the second half of the string
+ *
+ * @str: string whose second half will  be printed
+ *
+ * Return: 0 if successful
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i;
+	char c;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len = len - 1;
+
+	if (len % 2 == 0)
+	{
+		len = len / 2;
+		for (i = len; s[i] != '\0'; i++)
+		{
+			c = s[i];
+			_putchar(c);
+		}
+	}
+	else
+	{
+		len = (len - 1) / 2;
+		for (i = len; s[i] != '\0'; i++)
+		{
+			c = s[i];
+			_putchar(c);
+		}
+	}
+
+	_putchar('\n');
+}
