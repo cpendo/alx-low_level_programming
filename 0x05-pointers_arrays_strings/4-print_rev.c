aedc40f966702bfd247be50189097181eac89c39
cpendo <pendoc15@gmail.com>
2023-05-02 11:18:23 -0700

    Print a string in reverse followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: string to be reversed
+ *
+ * Return: 0 if succesful
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, i = 0;
+	char c;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i <= len)
+	{
+		c = s[len];
+		_putchar(c);
+		len--;
+	}
+
+	_putchar('\n');
+}
