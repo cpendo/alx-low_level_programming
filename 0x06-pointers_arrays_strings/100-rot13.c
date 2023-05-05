78a3c877219ed401edbce38bad8e4b22e43da9b8
cpendo <pendoc15@gmail.com>
2023-05-05 08:24:22 -0700

    Fix error in rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,13 +10,13 @@
 
 char *rot13(char *s)
 {
-	int len = 0, i;
+	int len = 0;
 
 	while (s[len] != '\0')
 	{
 		if ((s[len] >= 'A' && s[len] <= 'M') || (s[len] >= 'a' && s[len] <= 'm'))
 			s[len] += 13;
-		else if ((s[len] >= 'N' && s[len] <= 'Z') || (s[len] >= 'n' && s[len] <= 'z))
+		else if ((s[len] >= 'N' && s[len] <= 'Z') || (s[len] >= 'n' && s[len] <= 'z'))
 			s[len] -= 13;
 		len++;
 	}
