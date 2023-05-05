c0069066195e6c4109e54d148b784a39ea97f16b
cpendo <pendoc15@gmail.com>
2023-05-05 05:00:51 -0700

    Fix error in 7-leet file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,12 +12,12 @@ char *leet(char *s)
 {
 	int len = 0, i;
 	int lower[] = {97, 101, 111, 116, 108};
-	int upper[] = {65, 69, 79,84, 76};
-	int numbers[] = {48, 49, 51, 52, 55};
+	int upper[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
 
-	while ( s[len] != '\0')
+	while (s[len] != '\0')
 	{
-		for (i = 0, i < 5; i++)
+		for (i = 0; i < 5; i++)
 		{
 			if (s[len] == lower[i] || s[len] == upper[i])
 			{
