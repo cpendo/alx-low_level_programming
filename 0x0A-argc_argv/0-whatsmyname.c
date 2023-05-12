d0a1ba9924017b28a6334260e978e79f0116f109
cpendo <pendoc15@gmail.com>
2023-05-12 09:22:16 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: argument count
+ * @argv: array pointer to argument strings
+ *
+ * Return: int value or exit status
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
