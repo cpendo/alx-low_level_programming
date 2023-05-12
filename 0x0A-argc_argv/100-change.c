59ef8041ea4efcf5ef2d4218858a5ce45b3a4d43
cpendo <pendoc15@gmail.com>
2023-05-12 11:42:36 -0700

    Find change due

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,30 +24,28 @@ int main(int argc, char *argv[])
 	{
 		cents = atoi(argv[1]);
 
+		if (cents < 0)
+		{
+			printf("%d\n", 0);
+			return (1);
+		}
+
 		while (cents > 0)
 		{
-			switch (cents)
-			{
-				case 25:
-					cents -= 25;
-					break;
-				case 10:
-					cents -= 10;
-					break;
-				case 5:
-					cents -= 5;
-					break;
-				case 2:
-					cents -= 2;
-					break;
-				case 1:
-					cents -= 1;
-					break;
-			}
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else if (cents >= 1)
+				cents -= 1;
+
 			coins++;
 		}
 	}
 	printf("%d\n", coins);
 	return (0);
 }
-
