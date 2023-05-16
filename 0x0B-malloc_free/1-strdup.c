4a54f0ec6e225a033969a6ee9d4ad3b802404bb0
cpendo <pendoc15@gmail.com>
2023-05-16 11:54:43 -0700

    Copies string given as parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdli.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in
+ * memory containing a copy of the parameter
+ *
+ * @str: string to be copied
+ *
+ * Return: pointer to a newly allocated space or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		s[i] = str[len];
+
+	return (s);
+}
