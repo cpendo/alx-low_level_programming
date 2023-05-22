c52b72307f70b001d7f622f5573b19cde7f73464
cpendo <pendoc15@gmail.com>
2023-05-22 10:53:14 -0700

    Create a struct describing a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - decribes a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
