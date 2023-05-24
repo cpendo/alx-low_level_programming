2c85433a30208f7ea921b6b541d916ca6c0555d5
cpendo <pendoc15@gmail.com>
2023-05-24 14:00:44 -0700

    Print opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x", ((unsigned char *)main[i]));
+		printf("%02x", ((unsigned char *)main)[i]);
 		if (i != bytes - 1)
 			printf(" ");
 	}
