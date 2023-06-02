c435e0cc662e8a12a12dff8c8c26ee659d2fec55
cpendo <pendoc15@gmail.com>
2023-06-02 12:41:47 -0700

    Fix error in constructor function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: nothing
  */
 
-void before() __attribute__ ((constructor))
+void __attribute__ ((constructor)) before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
